75c499e4114b6d6004c2a57d74765211e3f11052
Noel <noelabdalkarim@gmail.com>
2025-11-29 18:18:32 +0200

    added API project

---
+++ 5isen_tracker_dll/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5isen_tracker_dll
+{
+    public class Class1
+    {
+    }
+}
---
+++ 5isen_tracker_web_app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _5isen_tracker_web_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
